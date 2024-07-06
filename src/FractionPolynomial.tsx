4cf94d0e92209a697550023788ae68a002d291b7
Chuenchat <chunchat02@gmail.com>
2024-07-06 10:45:15 +0700

    add rule desc

--- src/FractionPolynomial.tsx
+++ src/FractionPolynomial.tsx
@@ -14,7 +14,7 @@ export const FractionPolynomial: React.FC<{
   // find 2 correct answers
   const [find, setFind] = useState<number>(0);
   const [selected, setSelected] = useState<number[]>([]);
-  const nAnswers = 10;
+  const nAnswers = 12;
 
   const generateQuestion = () => {
     const a1 = Math.floor(Math.random() * 10);
@@ -52,11 +52,9 @@ export const FractionPolynomial: React.FC<{
     <>
       <div>
         {s && (
-          <div className="flex flex-col gap-5 items-center">
-            <div>
-              <div style={{ fontSize: 25, padding: 10, borderRadius: 15 }}>
-                <BlockMath>{polynomialDegree2(s.question)}</BlockMath>
-              </div>
+          <div className="flex flex-col gap-5 items-center pt-12">
+            <div className="text-3xl py-6">
+              <BlockMath>{polynomialDegree2(s.question)}</BlockMath>
             </div>
             <div className="flex flex-row flex-wrap gap-5 justify-center">
               {s.answer.map((ans, i) => {
@@ -71,7 +69,7 @@ export const FractionPolynomial: React.FC<{
                           ? "bg-green-200"
                           : "bg-red-200"
                         : "bg-gray-100"
-                    } p-5 rounded-lg w-48`}
+                    } p-5 rounded-lg w-40`}
                     onClick={() => {
                       if (!isSelected) {
                         setSelected([...selected, i]);
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import { FractionPolynomial } from "./FractionPolynomial";
 
+// กติกาของเกม
+// 1. เลือกตัวประกอบสำหรับพหุนามดีกรี 2 ที่ถูกต้อง
+// 2. ตอบถูก 1 ข้อได้ 1 คะแนน
+// 3. ตอบผิด 1 ข้อ จะหัก 1 คะแนน
+// 4. ตอบถูกทุกข้อ 2 ข้อ จะเป็นการเปลี่ยนข้อถัดไป
+
 export function Game() {
   const [score, setScore] = useState(0);
 
@@ -13,6 +19,15 @@ export function Game() {
         <h1 className="text-2xl">Score: {score}</h1>
       </div>
       <FractionPolynomial score={score} setScore={setScore} />
+      <div className="rounded-xl p-5 mt-20 m-5 text-left bg-slate-100">
+        <div className="font-bold">กติกาของเกม</div>
+        <ol>
+          <li>เลือกตัวประกอบสำหรับพหุนามดีกรี 2 ที่ถูกต้อง</li>
+          <li>ตอบถูก 1 ข้อได้ 1 คะแนน</li>
+          <li>ตอบผิด 1 ข้อ จะหัก 1 คะแนน</li>
+          <li>ตอบถูกทุกข้อ 2 ข้อ จะเป็นการเปลี่ยนข้อถัดไป</li>
+        </ol>
+      </div>
     </div>
   );
 }
